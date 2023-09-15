304513cafb38632afd82d1a28b647e22e4f2b343
vk401426 <vk401426@gmail.com>
2023-09-15 22:34:12 +0530

    JAVA : If else conditions  in a inherited class

--- result_student.java
+++ result_student.java
@@ -23,8 +23,8 @@ class detail
         System.out.print("\n\n Name : ");
         name=temp.readLine();
         System.out.print("\n Roll No : ");
-        roll_no=Integer.parseInt(temp.readLine());
-    }
+        roll_no=Integer.parseInt(temp.readLine());//1234  char ascii char
+      }
 
     void dis_detail()
     {
@@ -39,18 +39,35 @@ class marks extends detail
 {
     int total_marks;
     float average;
+    String grade;
 
     void calc_marks(int m1,int m2,int m3,int m4)
     {
         total_marks=m1+m2+m3+m4;
-        average=(float) (total_marks/4.0);
+        average=(float) (total_marks/4.0);// 43/4=10.75  =10.7500
 
     }
     
     void dis_marks()
-    {
+    {   
         System.out.println("\n\n *** MARKS ***");
         System.out.println("Totoal Marks : "+total_marks);
+
         System.out.println("Average : "+average);
+        if(average>=90 && average<=100)
+            grade="A+";
+        else if(average<90)
+            grade="A";
+        else if(average<80)
+            grade="B";
+        else if(average<60)
+            grade="c";
+        else if(average<40)
+            grade="D";
+        else if(average<30)
+            grade="Fail (First Attempt In Learning)";//congrats you are again here for  a year 
+
+        System.out.println(" \n Grade : "+grade);
+
     }
 }
