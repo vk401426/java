df6268923b54f8103a57c5a6c5fbb00813351b7a
vk401426 <vk401426@gmail.com>
2023-09-23 22:06:54 +0530

    Day 1 : Mini Project 1 :LIbrary

---
+++ library.java
@@ -0,0 +1,27 @@
+import java.io.*;
+class student{
+    void details(){
+        System.out.println("** Registration Student **");
+        System.out.println("Name :"+name);
+        System.out.println("Course :"+course);
+        System.out.println("Roll No :"+name);
+    }
+}
+class record{
+    void issue(){
+         System.out.println("** Issue Book **")
+    }
+    void register(){
+        System.out.println("** Report **");
+        System.out.println("Name Branch ID Book ISBN Issue_Date Return_Date ")
+    }
+}
+class books{
+    void book_record()
+    {
+        System.out.println(" ** Book record **");
+        System.out.println("Book : "+book_name);
+        System.out.println("ISBN : "+book_isbn);
+        System.out.println("Publisher : "+book_publisher);
+    }
+}
